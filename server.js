5bab151ea93192f9f3fbb6f406989fb143f6f123
TonyUCLA <TonyUCLA>
2017-01-11 14:41:08 -0800

    require code added to server file to test

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 
+// Server Routes
+require("./app/routing/api-routes.js")(app);
+require("./app/routing/html-routes.js")(app);
+
 var app = express();
 var PORT = 3000;
 
@@ -10,8 +14,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-
-// Server Routes
 
 // Starting the Server
 app.listen(PORT, function() {
