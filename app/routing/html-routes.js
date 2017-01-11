b4c39da7fd9c4db6bdf367b180aef132b7c4f444
TonyUCLA <TonyUCLA>
2017-01-11 14:03:15 -0800

    HTML Route added

--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,17 +1,16 @@
 // html-routes.js file should include two routes:
 // A GET Route to /survey which should display the survey page.
 // A default USE route that leads to home.html which displays the home page.
-var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
 
-
-var app = express();
-
+module.exports = function(app) {
 // GET route that goes to the Survey Page
-app.get('/survey', function (request, response) {
-    res.sendFile(path.join(__dirname, 'survey.html'));
-});
+    app.get('/survey', function (request, response) {
+        res.sendFile(path.join(__dirname, '/../public/survey.html'));
+    });
 
-// Default USE route that leads to Home Page.
-app.use(express.static(__dirname + '/home.html'));
+    // Default USE route that leads to Home Page.
+    app.use(function(request, response, next) {
+        res.sendFile(path.join(__dirname, '/../public/home.html'));
+    });
+};
