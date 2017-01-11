844059be9696620889fa10778dbce0a61f29bb98
TonyUCLA <TonyUCLA>
2017-01-11 13:51:19 -0800

    api routes setup

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -2,11 +2,22 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
+var friends = require(../data/friends.js);
 
-app.get("/api/friends", function(request, response) {
 
-});
+// showing friends data in json
+module.exports = function(app) {
+    app.get("/api/friends", function(request, response) {
+        response.json(friends);
+    });
+
+// post route for incoming survey results
+    app.post("/api/friends", function(request, response) {
+        var addedFriend = request.body;
+
+        friends.push(addedFriend);
+
+        response.json(addedFriend);
+    });
+}
 
