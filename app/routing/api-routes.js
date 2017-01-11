ce9907a5f2bc6b24139d070b89e25b5b61ea911f
TonyUCLA <TonyUCLA>
2017-01-10 18:35:16 -0800

    small edits

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -2,5 +2,11 @@
 // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 // A POST routes /api/friends. This will be used to handle incoming survey results. 
 // This route will also be used to handle the compatibility logic.
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-app.get("/api/")
+app.get("/api/friends", function(request, response) {
+
+});
+
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,10 +1,15 @@
 // html-routes.js file should include two routes:
 // A GET Route to /survey which should display the survey page.
 // A default USE route that leads to home.html which displays the home page.
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+
 var app = express();
 
 // GET route that goes to the Survey Page
-app.get('/survey', function (req, res) {
+app.get('/survey', function (request, response) {
     res.sendFile(path.join(__dirname, 'survey.html'));
 });
 
