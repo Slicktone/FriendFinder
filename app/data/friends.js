cc92f3bd59ecd3545d1bef5ab4333049f6ee0720
TonyUCLA <TonyUCLA>
2017-01-12 11:37:53 -0800

    Added Difference Function and new friend object

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,22 @@
+function totalDiff() {
+  for (var i = 0; i < friendInput.length; i++) {
+    var scoreDiff = parseInt(addedFriend.scores) - parseInt(friendInput.scores[i]);
+    console.log(scoreDiff);
+  }
+}
+totalDiff();
+
+
 var friendInput = [{
   "name":"",
   "photo":"",
   "scores":[]
 }];
+
+var addedFriend = {
+    "name":"",
+  "photo":"",
+  "scores":[]
+}
 
 module.exports = friendInput;
